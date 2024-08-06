a38cb8490100e26e293ad0f2182469047b24fad6
BlueSpace <gaoziyuan19@mails.ucas.ac.cn>
2024-08-06 10:54:50 +0800

    initialize megatron core_0.6.0

--- runtime/pretrain_t5.py
+++ runtime/pretrain_t5.py
@@ -1,25 +1,4 @@
-# coding=utf-8
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
-# The file has been adapted from the following Megatron-LM file:
-# https://github.com/NVIDIA/Megatron-LM/blob/v2.4/pretrain_t5.py
-# Git commit hash: 42c1cf4279acea5a554500dcb552211f44cbec45
-# We retain the following copyright from the original files:
-
-# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
 
 """Pretrain T5"""
 
@@ -27,70 +6,146 @@ from functools import partial
 
 import torch
 
-from megatron import (
+from megatron.training import (
     get_args,
     get_timers,
-    mpu,
+    get_tokenizer,
     print_rank_0
 )
-from megatron.data.dataset_utils import build_train_valid_test_datasets
-from megatron.model import FlexT5Model
+from megatron.core import mpu, tensor_parallel
+from megatron.core.enums import ModelType
+from megatron.core.models.T5 import T5Model
 from megatron.training import pretrain
-from megatron.utils import average_losses_across_data_parallel_group
+from megatron.training.utils import average_losses_across_data_parallel_group
+from megatron.training.arguments import core_transformer_config_from_args
+from megatron.core.datasets.blended_megatron_dataset_builder import BlendedMegatronDatasetBuilder
+from megatron.core.datasets.t5_dataset import T5MaskedWordPieceDataset, T5MaskedWordPieceDatasetConfig
+from megatron.core.models.T5.t5_spec import (get_t5_encoder_with_transformer_engine_block_spec,
+                                            get_t5_decoder_with_transformer_engine_block_spec,
+                                            get_t5_encoder_with_local_block_spec,
+                                            get_t5_decoder_with_local_block_spec)
+from megatron.legacy.model import T5Model as NonCoreT5Model
 
-def model_provider(pre_process=True, post_process=True):
-    """Build the model."""
+"""
+Pipeline parallelism for T5
+(Caveat: currently, mcore T5 model has not supported pipeline-parallelism)
+===========================
 
-    print_rank_0('building T5 model ...')
+T5 is a model architecture with both encoder and decoder blocks.
+Consequently, pipeline parallelism is implemented slightly differently
+compared to architectures like GPT and BERT.
 
-    model = FlexT5Model(num_tokentypes=0,
-                    parallel_output=True,
-                    pre_process=pre_process,
-                    post_process=post_process)
+In particular, when pipeline_model_parallel_world_size > 1, each stage
+either executes an encoder block or a decoder block. The
+--pipeline-model-parallel-split-rank argument controls the rank at which
+the split happens: all ranks lower than this argument execute the
+encoder block, and all ranks equal to or higher than this argument value
+execute the decoder block.
 
+In the encoder section of the model, only one tensor is sent downstream:
+the intermediate encoder_hidden_state. In the decoder section of the
+model, two tensors are sent downstream in the forward pass: the fully
+computed encoder_hidden_state, and the intermediate decoder_hidden_state.
+
+In particular, these are the shapes of the tensors sent between
+different workers:
+    If rank is in decoder section:
+        intermediate decoder_hidden_state (pre-transpose),
+        complete encoder_hidden_state (post-transpose).
+    If rank is at boundary between encoder and decoder sections:
+        complete encoder_hidden_state (post-transpose).
+    If rank is in encoder section:
+        intermediate encoder_hidden_state (pre-transpose).
+
+Additionally, we have code in the backward_step function in schedules.py
+to accumulate the encoder_hidden_state gradient across skip connections
+(encoder_hidden_state fed in as input to each layer in the decoder).
+"""
+
+def model_provider(pre_process=True, post_process=True, add_encoder=True, add_decoder=True) -> T5Model:
+    """Builds the model.
+
+    Args:
+        pre_process (bool, optional): Set to true if you need to compute embedings. Defaults to True.
+        post_process (bool, optional): Set to true if you need to want to compute output logits/loss. Defaults to True.
+        add_encoder (bool, optional): Defaults to True
+        add_decoder (bool, optional): Defaults to True
+    Returns:
+        T5Model: The returned T5 model
+    """
+
+
+    args = get_args()
+    config = core_transformer_config_from_args(args)
+    if args.use_mcore_models:
+        if args.transformer_impl=="local":
+            en_block_spec = get_t5_encoder_with_local_block_spec(args.encoder_num_layers)
+            de_block_spec = get_t5_decoder_with_local_block_spec(args.decoder_num_layers)
+        elif args.transformer_impl=="transformer_engine":
+            en_block_spec = get_t5_encoder_with_transformer_engine_block_spec(args.encoder_num_layers)
+            de_block_spec = get_t5_decoder_with_transformer_engine_block_spec(args.decoder_num_layers)
+        print_rank_0('building T5 model ...')
+        model = T5Model(
+            config=config,
+            transformer_encoder_layer_spec=en_block_spec,
+            transformer_decoder_layer_spec=de_block_spec,
+            vocab_size=args.padded_vocab_size,
+            max_sequence_length=args.max_position_embeddings,
+            pre_process=pre_process,
+            post_process=post_process,
+            fp16_lm_cross_entropy=args.fp16_lm_cross_entropy,
+            parallel_output=True,
+            share_embeddings_and_output_weights=not args.untie_embeddings_and_output_weights,
+            position_embedding_type=args.position_embedding_type,
+            rotary_percent=args.rotary_percent
+        )
+    else:
+        model = NonCoreT5Model(config=config,
+                        num_tokentypes=0,
+                        parallel_output=True,
+                        pre_process=pre_process,
+                        post_process=post_process,
+                        add_encoder=add_encoder,
+                        add_decoder=add_decoder)
     return model
 
-def t5_extended_attention_mask(attention_mask_list):
-
-    def attn_mask_postprocess(attn_mask):
-        # [b, 1, s, s]
-        extended_attention_mask = attn_mask.unsqueeze(1)
-        return extended_attention_mask
-
-    return [attn_mask_postprocess(attn_mask) for attn_mask in attention_mask_list]
-
-
-def t5_position_ids(token_ids):
-    # Create position ids
-    seq_length = token_ids.size(1)
-    position_ids = torch.arange(seq_length, dtype=torch.long,
-                                device=token_ids.device)
-    position_ids = position_ids.unsqueeze(0).expand_as(token_ids)
-
-    return position_ids
 
 def get_batch(data_iterator):
     """Build the batch."""
-    
-    ## Currently using synthetic data
-    args = get_args()
-    vocab_size = 30624
-    tokens_enc = torch.rand((args.micro_batch_size//mpu.get_op_dp_size(0), args.encoder_seq_length), requires_grad=False, device=torch.cuda.current_device()).long() * vocab_size
-    # tokens_dec = torch.rand((args.micro_batch_size, args.decoder_seq_length), requires_grad=False, device=torch.cuda.current_device()).long() * vocab_size
-    loss_mask =  (torch.rand((args.micro_batch_size//mpu.get_op_dp_size(-1), args.decoder_seq_length), requires_grad=False, device=torch.cuda.current_device()) < 0.5).float()
-    labels = torch.rand((args.micro_batch_size//mpu.get_op_dp_size(-1), args.decoder_seq_length), requires_grad=False, device=torch.cuda.current_device()).long() * vocab_size
-    enc_mask = (torch.rand((args.micro_batch_size, args.encoder_seq_length, args.encoder_seq_length), requires_grad=False, device=torch.cuda.current_device()) < 0.5)
-    dec_mask = (torch.rand((args.micro_batch_size, args.decoder_seq_length, args.decoder_seq_length), requires_grad=False, device=torch.cuda.current_device()) < 0.5)
-    enc_dec_mask = (torch.rand((args.micro_batch_size, args.decoder_seq_length, args.encoder_seq_length), requires_grad=False, device=torch.cuda.current_device()) < 0.5)
 
-    return tokens_enc, loss_mask, labels, enc_mask, dec_mask, enc_dec_mask
+    keys = ['text_enc', 'text_dec', 'labels', 'loss_mask',
+            'enc_mask', 'dec_mask', 'enc_dec_mask']
+    datatype = torch.int64
+
+    # Broadcast data.
+    if data_iterator is not None:
+        data = next(data_iterator)
+    else:
+        data = None
+    data_b = tensor_parallel.broadcast_data(keys, data, datatype)
+
+    # Unpack.
+    tokens_enc = data_b['text_enc'].long()
+    tokens_dec = data_b['text_dec'].long()
+    labels = data_b['labels'].long()
+    loss_mask = data_b['loss_mask'].float()
+
+    enc_mask = (data_b['enc_mask'] < 0.5)
+    dec_mask = (data_b['dec_mask'] < 0.5)
+    enc_dec_mask = (data_b['enc_dec_mask'] < 0.5)
+
+    return tokens_enc, tokens_dec, loss_mask, labels, \
+           enc_mask, dec_mask, enc_dec_mask
 
 
-def loss_func(loss_mask, output_tensor):
-    
-    lm_loss_ = output_tensor["output"].float()
+def loss_func(loss_mask: torch.Tensor, output_tensor: torch.Tensor):
+    """Loss function.
 
-    lm_loss_ = lm_loss_.float()
+    Args:
+        loss_mask (torch.Tensor): Used to mask out some portions of the loss
+        output_tensor (torch.Tensor): The tensor with the losses
+    """   
+    lm_loss_ = output_tensor.float()
     lm_loss = torch.sum(
         lm_loss_.view(-1) * loss_mask.reshape(-1)) / loss_mask.sum()
 
@@ -99,72 +154,87 @@ def loss_func(loss_mask, output_tensor):
 
     return loss, {'lm loss': averaged_losses[0]}
 
-def forward_step(data_iterator, model, extra_tensors_):
-    """Forward step."""
+
+def forward_step(data_iterator, model: T5Model):
+    """Forward training step.
+
+    Args:
+        data_iterator : Input data iterator
+        model (T5Model): The T5 Model
+    """
+
     args = get_args()
     timers = get_timers()
 
     # Get the batch.
-    timers('batch-generator').start()
-    tokens_enc, loss_mask, lm_labels, enc_mask, dec_mask, enc_dec_mask = get_batch(data_iterator)
+    timers('batch generator', log_level=2).start()
+    tokens_enc, tokens_dec, loss_mask, lm_labels, enc_mask, dec_mask, enc_dec_mask \
+        = get_batch(data_iterator)
+    timers('batch generator').stop()
 
-    # Converting the attention masks to proper parameter settings
-    encoder_attn_mask, decoder_attn_mask, encoder_decoder_attn_mask = t5_extended_attention_mask([enc_mask, dec_mask, enc_dec_mask])
-    encoder_position_ids = t5_position_ids(tokens_enc)
-    # decoder_position_ids = t5_position_ids(tokens_dec)
+    # Forward model lm_labels
+    output_tensor = model(tokens_enc,
+                        tokens_dec,
+                        enc_mask,
+                        dec_mask,
+                        enc_dec_mask,
+                        lm_labels=lm_labels)
 
-    input_tensors = {}
-    input_tensors["enc_input_ids"] = tokens_enc
-    input_tensors["enc_position_ids"] = encoder_position_ids
-    # input_tensors["dec_input_ids"] = tokens_dec
-    # input_tensors["dec_position_ids"] = decoder_position_ids
+    return output_tensor, partial(loss_func, loss_mask)
 
-    extra_tensors = {}
-    extra_tensors["labels"] = lm_labels
-    extra_tensors["enc_attention_mask"] = encoder_attn_mask
-    extra_tensors["dec_attention_mask"] = decoder_attn_mask
-    extra_tensors["enc_dec_attention_mask"] = encoder_decoder_attn_mask
 
-    if extra_tensors_ is not None:
-        for key in extra_tensors_:
-            extra_tensors[key] = extra_tensors_[key]
-            
-    timers('batch-generator').stop()
+def train_valid_test_datasets_provider(train_val_test_num_samples: int):
+    """Build the train test and validation datasets.
 
-    if mpu.is_pipeline_last_stage():
-        output_tensor = model(input_tensors, extra_tensors)
-        ouput_extra_tensors = None
-    else:
-        output_tensor, ouput_extra_tensors = model(input_tensors, extra_tensors)
+    Args:
+        train_val_test_num_samples : A list containing the number of samples in train test and validation.
+    """
+    args = get_args()
 
-    return output_tensor, ouput_extra_tensors, partial(loss_func, loss_mask)    
+    tokenizer = get_tokenizer()
 
-def train_valid_test_datasets_provider(train_val_test_num_samples):
-    """Build train, valid, and test datasets."""
+    config = T5MaskedWordPieceDatasetConfig(
+        random_seed=args.seed,
+        sequence_length=args.encoder_seq_length,
+        sequence_length_decoder=args.decoder_seq_length,
+        blend=args.data_path,
+        blend_per_split=[
+            args.train_data_path,
+            args.valid_data_path,
+            args.test_data_path,
+        ],
+        split=args.split,
+        path_to_cache=args.data_cache_path,
+        mock=False,
+        tokenizer=tokenizer,
+        masking_probability=args.mask_prob,
+        short_sequence_probability=args.short_seq_prob,
+        masking_max_ngram=10,
+        masking_do_full_word=True,
+        masking_do_permutation=False,
+        masking_use_longer_ngrams=False,
+        masking_use_geometric_distribution=True,
+    )
 
-    ## Currently using synthetic data
-    return None, None, None
-        
-    # args = get_args()
-    # print_rank_0('> building train, validation, and test datasets '
-    #              'for T5 ...')
-    # train_ds, valid_ds, test_ds = build_train_valid_test_datasets(
-    #     data_prefix=args.data_path,
-    #     data_impl=args.data_impl,
-    #     splits_string=args.split,
-    #     train_valid_test_num_samples=train_val_test_num_samples,
-    #     max_seq_length=args.encoder_seq_length,
-    #     max_seq_length_dec=args.decoder_seq_length,
-    #     masked_lm_prob=args.mask_prob,
-    #     short_seq_prob=args.short_seq_prob,
-    #     seed=args.seed,
-    #     skip_warmup=(not args.mmap_warmup),
-    #     dataset_type='t5')
-    # print_rank_0("> finished creating T5 datasets ...")
-    # return train_ds, valid_ds, test_ds
+    print_rank_0('> building train, validation, and test datasets '
+                 'for T5 ...')
+
+    train_ds, valid_ds, test_ds = BlendedMegatronDatasetBuilder(
+        T5MaskedWordPieceDataset,
+        train_val_test_num_samples,
+        lambda: mpu.get_tensor_model_parallel_rank() == 0,
+        config,
+    ).build()
+
+    print_rank_0("> finished creating T5 datasets ...")
+
+    return train_ds, valid_ds, test_ds
 
 
 if __name__ == "__main__":
-    forward_step_func = forward_step
-    pretrain(train_valid_test_datasets_provider, model_provider, forward_step_func,
-             args_defaults={'tokenizer_type': 'BertWordPieceLowerCase'})
+
+    # Temporary for transition to core datasets
+    train_valid_test_datasets_provider.is_distributed = True
+
+    pretrain(train_valid_test_datasets_provider, model_provider, ModelType.encoder_and_decoder,
+             forward_step, args_defaults={'tokenizer_type': 'BertWordPieceLowerCase'})
