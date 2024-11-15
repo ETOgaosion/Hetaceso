477f7f63991255a3feb2e155653537e2c6c97e1c
Blue Space <gaoziyuan19@mails.ucas.ac.cn>
2024-11-15 06:31:58 +0000

    test on multiple machines

--- runtime/megatron/core/pipeline_parallel/p2p_communication.py
+++ runtime/megatron/core/pipeline_parallel/p2p_communication.py
@@ -1209,40 +1209,51 @@ def initialize_weights_sharing(models):
                                 num_ranks_in_send_stage = len(ranks_in_send_stage)
                                 num_ranks_in_receive_stage = len(ranks_in_receive_stage)
 
-                                tp_size, dp_size = mpu.get_op_tp_size(op.op_index), mpu.get_op_dp_size(op.op_index)
-                                tp_size_next, dp_size_next = mpu.get_op_tp_size(op_index), mpu.get_op_dp_size(op_index)
+                                tp_size, cp_size, dp_size = mpu.get_op_tp_size(op.op_index), mpu.get_op_cp_size(op.op_index), mpu.get_op_dp_size(op.op_index)
+                                tp_size_next, cp_size_next, dp_size_next = mpu.get_op_tp_size(op_index), mpu.get_op_cp_size(op_index), mpu.get_op_dp_size(op_index)
 
                                 for i in range(num_ranks_in_send_stage):
                                     if ranks_in_send_stage[i] == rank:
-                                        dp_id = i // tp_size
+                                        dp_id = i // (cp_size * tp_size)
+                                        cp_id = (i // tp_size) % cp_size
                                         tp_id = i % tp_size
 
-                                next_dp_id = [dp_id]
-                                next_tp_id = [tp_id]
+                                next_dp_id = []
+                                next_cp_id = []
+                                next_tp_id = []
 
                                 if tp_size_next > tp_size:
                                     ratio = tp_size_next // tp_size
-                                    next_tp_id = range(tp_id * ratio, (tp_id + 1)*ratio)                                    
+                                    next_tp_id = range(tp_id * ratio, (tp_id + 1)*ratio)
                                 if tp_size_next < tp_size:
                                     ratio = tp_size // tp_size_next
-                                    next_tp_id = [tp_id // ratio]  
+                                    if tp_id % ratio == 0:
+                                        next_tp_id = [cp_id // ratio]
+                                if cp_size_next > cp_size:
+                                    ratio = cp_size_next // cp_size
+                                    next_cp_id = range(cp_id * ratio, (cp_id + 1)*ratio)
+                                if cp_size_next < cp_size:
+                                    ratio = cp_size // cp_size_next
+                                    if cp_id % ratio == 0:
+                                        next_cp_id = [cp_id // ratio]
                                 if dp_size_next > dp_size:
                                     ratio = dp_size_next // dp_size
-                                    next_dp_id = range(dp_id * ratio, (dp_id + 1)*ratio)                                      
+                                    next_dp_id = range(dp_id * ratio, (dp_id + 1)*ratio)
                                 if dp_size_next < dp_size:
                                     ratio = dp_size // dp_size_next
                                     if dp_id % ratio == 0:
-                                        next_dp_id = [dp_id // ratio] 
-                                    else:
-                                        next_dp_id = []
+                                        next_dp_id = [dp_id // ratio]
+                                
+                                print(f'rank {rank} root op {op.op_index} key {key} sharing with op {op_index} next_dp_id {next_dp_id} next_cp_id {next_cp_id} next_tp_id {next_tp_id} ranks_in_send_stage {ranks_in_send_stage} ranks_in_receive_stage {ranks_in_receive_stage}')
 
                                 op.shared_weights_info[key]["sharing_weights_with_ranks"][op_index] = []
                                 if len(next_dp_id) > 0:
                                     for _dp_id in next_dp_id:
-                                        tmp_list = []
-                                        for _tp_id in next_tp_id:
-                                            tmp_list.append(ranks_in_receive_stage[_dp_id * tp_size_next + _tp_id])
-                                        op.shared_weights_info[key]["sharing_weights_with_ranks"][op_index].append(list(tmp_list))
+                                        for _cp_id in next_cp_id:
+                                            tmp_list = []
+                                            for _tp_id in next_tp_id:
+                                                tmp_list.append(ranks_in_receive_stage[_dp_id * tp_size_next * cp_size_next + _cp_id * tp_size_next + _tp_id])
+                                            op.shared_weights_info[key]["sharing_weights_with_ranks"][op_index].append(list(tmp_list))
                     else:
                         assert len(op.shared_weights_info[key]["sharing_with_ops"]) == 1
                         op_index = op.shared_weights_info[key]["sharing_with_ops"][0]
@@ -1257,37 +1268,47 @@ def initialize_weights_sharing(models):
                             num_ranks_in_send_stage = len(ranks_in_send_stage)
                             num_ranks_in_receive_stage = len(ranks_in_receive_stage)
 
-                            tp_size, dp_size = mpu.get_op_tp_size(op.op_index), mpu.get_op_dp_size(op.op_index)
-                            tp_size_next, dp_size_next = mpu.get_op_tp_size(op_index), mpu.get_op_dp_size(op_index)
+                            tp_size, cp_size, dp_size = mpu.get_op_tp_size(op.op_index), mpu.get_op_cp_size(op.op_index), mpu.get_op_dp_size(op.op_index)
+                            tp_size_next, cp_size_next, dp_size_next = mpu.get_op_tp_size(op_index), mpu.get_op_cp_size(op_index), mpu.get_op_dp_size(op_index)
 
                             for i in range(num_ranks_in_receive_stage):
                                 if ranks_in_receive_stage[i] == rank:
-                                    dp_id = i // tp_size
+                                    dp_id = i // (tp_size * cp_size)
+                                    cp_id = (i // tp_size) % cp_size
                                     tp_id = i % tp_size
 
                             next_dp_id = [dp_id]
+                            next_cp_id = [cp_id]
                             next_tp_id = [tp_id]
 
                             if tp_size_next > tp_size:
                                 ratio = tp_size_next // tp_size
-                                next_tp_id = range(tp_id * ratio, (tp_id + 1)*ratio)                                    
+                                next_tp_id = range(tp_id * ratio, (tp_id + 1)*ratio)
                             if tp_size_next < tp_size:
                                 ratio = tp_size // tp_size_next
-                                next_tp_id = [tp_id // ratio]  
+                                next_tp_id = [tp_id // ratio]
+                            if cp_size_next > cp_size:
+                                ratio = cp_size_next // cp_size
+                                next_cp_id = range(cp_id * ratio, (cp_id + 1)*ratio)
+                            if cp_size_next < cp_size:
+                                ratio = cp_size // cp_size_next
+                                next_cp_id = [cp_id // ratio]
                             if dp_size_next > dp_size:
                                 ratio = dp_size_next // dp_size
-                                next_dp_id = [dp_id * ratio]                                 
+                                next_dp_id = range(dp_id * ratio, (dp_id + 1)*ratio)
                             if dp_size_next < dp_size:
                                 ratio = dp_size // dp_size_next
-                                next_dp_id = [dp_id // ratio]   
+                                next_dp_id = [dp_id // ratio]
 
+                            print(f'rank {rank} op {op.op_index} key {key} sharing with op {op_index} next_dp_id {next_dp_id} next_cp_id {next_cp_id} next_tp_id {next_tp_id} ranks_in_send_stage {ranks_in_send_stage} ranks_in_receive_stage {ranks_in_receive_stage}')
                             op.shared_weights_info[key]["sharing_weights_with_ranks"][op_index] = []
 
                             for _dp_id in next_dp_id:
-                                tmp_list = []
-                                for _tp_id in next_tp_id:
-                                    tmp_list.append(ranks_in_send_stage[_dp_id * tp_size_next + _tp_id])
-                                op.shared_weights_info[key]["sharing_weights_with_ranks"][op_index].append(list(tmp_list))
+                                for _cp_id in next_cp_id:
+                                    tmp_list = []
+                                    for _tp_id in next_tp_id:
+                                        tmp_list.append(ranks_in_send_stage[_dp_id * tp_size_next * cp_size_next + _cp_id * tp_size_next + _tp_id])
+                                    op.shared_weights_info[key]["sharing_weights_with_ranks"][op_index].append(list(tmp_list))
 
     # send & receive tensors
     for model in models:
