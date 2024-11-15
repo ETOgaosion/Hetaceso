71d2ea1788fef2ffbf73e6c6735113d9ab2b3d85
Blue Space <gaoziyuan19@mails.ucas.ac.cn>
2024-11-15 10:27:59 +0000

    test on multiple machines

--- runtime/megatron/core/pipeline_parallel/p2p_communication.py
+++ runtime/megatron/core/pipeline_parallel/p2p_communication.py
@@ -1260,6 +1260,7 @@ def initialize_weights_sharing(models):
                                             for _tp_id in next_tp_id:
                                                 tmp_list.append(ranks_in_receive_stage[_dp_id * tp_size_next * cp_size_next + _cp_id * tp_size_next + _tp_id])
                                             op.shared_weights_info[key]["sharing_weights_with_ranks"][op_index].append(list(tmp_list))
+                                print(f'rank {rank} op.shared_weights_info[key]["sharing_weights_with_ranks"][op_index]: {op.shared_weights_info[key]["sharing_weights_with_ranks"][op_index]}')
                     else:
                         assert len(op.shared_weights_info[key]["sharing_with_ops"]) == 1
                         op_index = op.shared_weights_info[key]["sharing_with_ops"][0]
@@ -1316,6 +1317,8 @@ def initialize_weights_sharing(models):
                                         for _tp_id in next_tp_id:
                                             tmp_list.append(ranks_in_send_stage[_dp_id * tp_size_next * cp_size_next + _cp_id * tp_size_next + _tp_id])
                                         op.shared_weights_info[key]["sharing_weights_with_ranks"][op_index].append(list(tmp_list))
+                            
+                            print(f'rank {rank} op.shared_weights_info[key]["sharing_weights_with_ranks"][op_index]: {op.shared_weights_info[key]["sharing_weights_with_ranks"][op_index]}')
 
     # send & receive tensors
     for model in models:
