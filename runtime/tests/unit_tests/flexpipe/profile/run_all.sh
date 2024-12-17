be6cd39fd9637deb954c598bbe430aec4a42f0a7
BlueSpace <gaoziyuan19@mails.ucas.ac.cn>
2024-12-17 05:07:30 +0000

    use run all

--- runtime/tests/unit_tests/flexpipe/profile/run_all.sh
+++ runtime/tests/unit_tests/flexpipe/profile/run_all.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+MACHINE=${1:-0}
 
 echo "========== test 350M ==========="
-cd 350M && ./run_all.sh
+cd 350M && ./run_all.sh $MACHINE
 echo "========== test 1_3B ==========="
-cd ../1_3B && ./run_all.sh
+cd ../1_3B && ./run_all.sh $MACHINE
 echo "========== test 2_6B ==========="
-cd ../2_6B && ./run_all.sh
-echo "========== test 6_7B ==========="
-cd ../6_7B && ./run_all.sh
-echo "========== test 13B ==========="
-cd ../13B && ./run_all.sh
+cd ../2_6B && ./run_all.sh $MACHINE
+# echo "========== test 6_7B ==========="
+# cd ../6_7B && ./run_all.sh
+# echo "========== test 13B ==========="
+# cd ../13B && ./run_all.sh
