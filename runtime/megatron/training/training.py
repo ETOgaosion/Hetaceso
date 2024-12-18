df5b4159d4dd9ca96dd30ffebee948f38428752d
BlueSpace <gaoziyuan19@mails.ucas.ac.cn>
2024-12-18 04:09:15 +0000

    profile inside ring

--- runtime/megatron/training/training.py
+++ runtime/megatron/training/training.py
@@ -734,6 +734,7 @@ def training_log(loss_dict, total_loss_dict, learning_rate, decoupled_learning_r
         'TEAttnFwd',
         'TEAttnBwd',
         'TERingAttnCoreLoopFwd',
+        'TERingAttnCoreLoopInnerCalcFwd',
         'TEFlashAttnFwd',
         'TEFlashAttnBwd',
         'TEA2A',
