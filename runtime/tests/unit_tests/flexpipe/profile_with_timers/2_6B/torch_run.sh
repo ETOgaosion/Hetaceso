ab8e97907d3038511fb9f6675314596bf6b8d48c
BlueSpace <gaoziyuan19@mails.ucas.ac.cn>
2024-12-20 06:54:24 +0000

    disable timer in torch profile

--- runtime/tests/unit_tests/flexpipe/profile_with_timers/2_6B/torch_run.sh
+++ runtime/tests/unit_tests/flexpipe/profile_with_timers/2_6B/torch_run.sh
@@ -115,7 +115,7 @@ mkdir -p logs
 mkdir -p logs/csv
 
 # export USE_FUSED_ATTN=1 && \
-export TIMERS_LOG_LEVEL=2 && \
+export TIMERS_LOG_LEVEL=0 && \
 export USE_FLASH_ATTN=1 && \
 torchrun $DISTRIBUTED_ARGS \
     pretrain_gpt.py \
