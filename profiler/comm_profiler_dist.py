8fc01c072c6a633d7c89648bdb1db8b2ab4c5350
BlueSpace <gaoziyuan19@mails.ucas.ac.cn>
2025-01-10 08:27:12 +0000

    to test specific ring ops

--- profiler/comm_profiler_dist.py
+++ profiler/comm_profiler_dist.py
@@ -413,6 +413,7 @@ def run_profile(args, task):
     
     device = args.rank % torch.cuda.device_count()
     visible_devices = os.getenv("CUDA_VISIBLE_DEVICES", "").split(",")
+    print(f'device{device}, visible_devices: {visible_devices}')
     torch.cuda.set_device(int(visible_devices[device]))
     torch.distributed.init_process_group(backend="nccl", world_size=world_size, rank=args.rank)
     
