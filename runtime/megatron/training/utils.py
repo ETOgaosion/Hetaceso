ec0103972ba115b860ec64baeb347a90624ef2de
Ubuntu <ubuntu@ip-172-31-37-189.ec2.internal>
2025-02-23 12:41:45 +0000

    debug

--- runtime/megatron/training/utils.py
+++ runtime/megatron/training/utils.py
@@ -312,6 +312,7 @@ def get_batch_on_this_tp_rank(data_iterator):
 
     def _broadcast(item):
        if item is not None:
+           print(f"[rank {torch.distributed.get_rank()}] tp src_rank: {mpu.get_tensor_model_parallel_src_rank()}. {torch.distibuted.get_process_group_ranks(mpu.get_tensor_model_parallel_group())}")
            torch.distributed.broadcast(item, mpu.get_tensor_model_parallel_src_rank(), group=mpu.get_tensor_model_parallel_group())
 
     local_micro_batch_size = args.micro_batch_size // mpu.get_data_parallel_world_size()
@@ -333,7 +334,7 @@ def get_batch_on_this_tp_rank(data_iterator):
             'position_ids': data["position_ids"].cuda(non_blocking = True)
         }
         if mpu.is_pipeline_first_stage() and mpu.get_tensor_model_parallel_world_size() > 1:
-            print(f'[rank {torch.distributed.get_rank()}] broadcast tokens')
+            print(f'[rank {torch.distributed.get_rank()}] broadcast tokens. shape: {batch["tokens"].size()}')
             _broadcast(batch['tokens'])
             print(f'[rank {torch.distributed.get_rank()}] broadcast attention_mask')
             _broadcast(batch['attention_mask'])
@@ -362,7 +363,7 @@ def get_batch_on_this_tp_rank(data_iterator):
         position_ids=torch.empty((local_micro_batch_size,args.seq_length), dtype = torch.int64 , device = torch.cuda.current_device())
  
         if mpu.is_pipeline_first_stage():
-            print(f'[rank {torch.distributed.get_rank()}] broadcast tokens')
+            print(f'[rank {torch.distributed.get_rank()}] broadcast tokens. shape: {tokens.size()}')
             _broadcast(tokens)
             print(f'[rank {torch.distributed.get_rank()}] broadcast attention_mask')
             _broadcast(attention_mask)
