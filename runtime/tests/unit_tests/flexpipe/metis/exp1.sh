90ae2f9afa829ee1deff79ce937edea04b08ceb7
Ubuntu <ubuntu@ip-172-31-37-189.ec2.internal>
2025-03-24 08:45:18 +0000

    exp1

--- runtime/tests/unit_tests/flexpipe/metis/exp1.sh
+++ runtime/tests/unit_tests/flexpipe/metis/exp1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-./run_rank.sh -n 0 -m GPT_350M -s 2048  -u 2 -g 512  -f ./config/exp1/GPT_350M_seq-2048_a10g_cp-2.json 
+./run_rank.sh -n 0 -m GPT_350M -s 4096  -u 2 -g 1024  -f ./config/exp1/GPT_350M_seq-4096_a10g_cp-2.json 
