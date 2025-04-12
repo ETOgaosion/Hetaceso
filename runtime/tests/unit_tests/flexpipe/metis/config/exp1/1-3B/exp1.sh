c079409bbe009b3be627788d82dd71fdb48ad91b
Ubuntu <ubuntu@ip-172-31-37-189.ec2.internal>
2025-04-12 07:13:43 +0000

    debug

---
+++ runtime/tests/unit_tests/flexpipe/metis/config/exp1/1-3B/exp1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+# "Usage: $0 -n <node_rank> -m <model_name> -s <sequence_length> -u <micro_batch_size> -g <global_batch_size> -f <flex_config>"
+./run_rank.sh -n 0 -m GPT_1-3B -s 4096  -u 4 -g 1024  -f ./config/exp1/1-3B/GPT_1-3B_seq-4096_a10g_dps-1_l40s_dps-3.json
